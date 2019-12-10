0a857dfc47e11d23cd7c88eacfcbc0a5909dfdd1
liuxuwen10140721 <liu.xuwen@zte.com.cn>
2019-12-10 16:55:32 +0800

    fix it

--- myWorker.js
+++ myWorker.js
@@ -3,14 +3,18 @@ importScripts("factorial.js");
 self.onmessage = function(e) {
     console.log('Worker: Message received from main script');
     console.log(e.data);
+    console.log(self.navigator);
+    console.log(self.location);
     if (e.data.type === 'data') {
         let value = +e.data.data;
         if (isNaN(value)) {
-            postMessage('Please write number');
+            self.postMessage('Please write number');
         } else {
             let workerResult = factorial(value);
             console.log('Worker: Posting message back to main script');
-            postMessage(workerResult);
+            setTimeout(() => {
+                self.postMessage(workerResult);
+            },2000);
         }
     } else {
         self.close();
