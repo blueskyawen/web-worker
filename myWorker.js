b3dbfd72dd8058e8329097e72e3af0a9cb38e868
liuxuwen10140721 <liu.xuwen@zte.com.cn>
2019-12-10 16:41:24 +0800

    fix

--- myWorker.js
+++ myWorker.js
@@ -3,7 +3,7 @@ importScripts("factorial.js");
 self.onmessage = function(e) {
     console.log('Worker: Message received from main script');
     console.log(e.data);
-    if (e.data.type === 'close') {
+    if (e.data.type === 'data') {
         let value = +e.data.data;
         if (isNaN(value)) {
             postMessage('Please write number');
